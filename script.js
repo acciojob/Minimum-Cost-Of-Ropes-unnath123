49a6beced717b337812ffb00e93fff904ad90aa9
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-08-16 18:09:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function calculateMinCost() {
+function calculateMinCost(arr) {
   //your code here
-  
-  
-  
+  let ans=0;
+  let arr1=[]; 
+	for(let i=0;i<5;i++){
+		arr.sort((a,b)=>a-b);
+		if(arr.length>1){
+		let sum=arr[0]+arr[1];
+		ans=ans+sum;
+		console.log(sum);
+		arr.splice(0,2);
+		arr.unshift(sum);
+		
+			console.log(arr.sort((a,b)=>a-b));
+			
+	}
+	else{
+		return ans;
+	}
+	}
+  return ans;
 }  
